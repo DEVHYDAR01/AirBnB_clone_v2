db1221cdce02f0d1c475bfdf6fce011afad93379
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-02-22 02:56:48 +0100

    This commit will push all files

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ test file """
 import os.path
-import time
-from fabric.operations import run, put, sudo
+import os
 from fabric.api import *
+from fabric.operations import run, put, sudo
+import time
 env.hosts = ['35.185.103.0', '35.237.21.105']
 
 
@@ -45,3 +46,18 @@ def deploy():
         return val
     except:
         return False
+
+
+def do_clean(number=0):
+    number = 1 if int(number) == 0 else int(number)
+
+    files = sorted(os.listdir("versions"))
+    [files.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(j) for j in files)]
+
+    with cd("data/web_static/releases"):
+        files = run("ls -tr").split()
+        files = [j for j in files if "web_static_" in j]
+        [files.pop() for i in range(number)]
+        [run("sudo rm -rf ./{}".format(j)) for j in files]
