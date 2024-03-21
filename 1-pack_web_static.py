dbcf34da9701a9d1e3ded42577d1ea62795d9a77
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-03-21 12:20:13 +0100

    This commit will push web static deployment

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive"""
+"""
+Fabric script to genereate tgz archive
+execute: fab -f 1-pack_web_static.py do_pack
+"""
 
-from fabric.api import local
-import time
+from datetime import datetime
+from fabric.api import *
 
 
 def do_pack():
-    time_string = time.strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(time_string))
-        return ("versions/web_static_{}.tgz".format(time_string))
-    except:
+    """
+    making an archive on web_static folder
+    """
+
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
         return None
